8f88958a397d9bcddf951abe9a3da70c7ac7ec9d
Jun-cloud <kjy8827@gmail.com>
2020-06-09 21:07:37 +0900

    Create Add Method

--- main.go
+++ main.go
@@ -17,4 +17,16 @@ func main() {
 	} else {
 		fmt.Println(definition)
 	}
+
+	err = dictionary.Add("hello", "Greeting")
+	if err != nil {
+		fmt.Println(err)
+	}
+	definition, err = dictionary.Search("hello")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
+
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,7 +5,8 @@ import "errors"
 // Dictionary - 별칭이 Dictionary인 map
 type Dictionary map[string]string
 
-var errNotFound = "Not Found"
+var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 // Search - map에서 키에 해당하는 값을 return
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,5 +15,18 @@ func (d Dictionary) Search(word string) (string, error) {
 	if exists {
 		return value, nil
 	}
-	return "", errors.New(errNotFound)
+	return "", errNotFound
+}
+
+// Add - map에 값이 존재하지 않으면 추가
+func (d Dictionary) Add(word, def string) error {
+	// map에 값이 있는지 확인하기 위해 만들어 놓은 Search() Method를 사용
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
